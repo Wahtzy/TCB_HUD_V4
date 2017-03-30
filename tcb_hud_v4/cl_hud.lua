7b86bceb325adca3779fab939232415b7127dd77
Wahtzy <Davidkochg@gmail.com>
2017-03-30 13:01:05 +0200

    Minor fix

--- tcb_hud_v4/cl_hud.lua
+++ tcb_hud_v4/cl_hud.lua
@@ -1,6 +1,7 @@
 /*---------------------------------------------------------------------------
 	
-	Creator: TheCodingBeast - TheCodingBeast.com
+	WattyHud v0.1
+	Based on the works of: TheCodingBeast - TheCodingBeast.com
 	This work is licensed under the Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International License. 
 	To view a copy of this license, visit http://creativecommons.org/licenses/by-nc-sa/4.0/
 	
