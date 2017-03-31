34baac168d0705c0bd66721d1822724f6de79228
Wahtzy <Davidkochg@gmail.com>
2017-03-31 22:31:31 +0200

    Fixed server not streaming icons

--- watty_hud_v01/sv_resource.lua
+++ watty_hud_v01/sv_resource.lua
@@ -2,7 +2,7 @@
 	Name: Load Resources
 -----------------------------------------------------------]]
 resource.AddFile( "resource/fonts/cour.ttf" )
---resource.AddFile( "materials/icon/ic_heart.png" )
---resource.AddFile( "materials/icon/ic_person.png" )
---resource.AddFile( "materials/icon/ic_page.png" )
---resource.AddFile( "materials/icon/ic_money.png" )
+resource.AddFile( "materials/icon/ic_heart.png" )
+resource.AddFile( "materials/icon/ic_person.png" )
+resource.AddFile( "materials/icon/ic_page.png" )
+resource.AddFile( "materials/icon/ic_money.png" )
